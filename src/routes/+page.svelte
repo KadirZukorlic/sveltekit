78b2b60567d03199fdf76f1338b3826faf92915f
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-10-15 01:16:49 +0200

    Basic routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to home</h1>
+
+<a href="/about">go to about</a>
+<a href="/profile">go to My Profile</a>
