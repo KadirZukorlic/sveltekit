4f237cc8eb91f4d15c02f8c938ac225bd6583c60
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-10-15 20:08:29 +0200

    Programmatically navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,23 @@
+<script>
+    import { goto, beforeNavigate, afterNavigate } from '$app/navigation'
+    const orderHandler = () => {
+        console.log('Placing your order!')
+        goto('/products', { replaceState: true })
+    }
+
+    beforeNavigate((navigation) => {
+        console.log({ before: navigation })
+    })
+    afterNavigate((navigation) => {
+        console.log({ after: navigation })
+    })
+</script>
+
 <h1>Welcome to home</h1>
 
 <a href="/about">About</a>
 <a href="/products">Products</a>
 <a href="/blog">Blog</a>
 <a href="/profile">Profile</a>
+
+<button on:click={orderHandler}>Place Order</button>
