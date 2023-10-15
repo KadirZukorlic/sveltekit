bc29f9242dba7f9c335a6b20c5133a8850ff3a9b
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-10-15 02:58:16 +0200

    Nested Dynamic Routes

--- src/routes/products/[productId]/+page.svelte
+++ src/routes/products/[productId]/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { page } from '$app/stores'
 
-    const productId = $page.params.id
+    const productId = $page.params.productId
 </script>
 
 <h1>Details about product {productId}</h1>
---
+++ src/routes/products/[productId]/reviews/[reviewId]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { page } from '$app/stores'
+
+    const { productId, reviewId } = $page.params
+</script>
+
+<h2>Review {reviewId} for {productId}</h2>
