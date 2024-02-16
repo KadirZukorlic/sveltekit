e469c81b09a92f1455ebedc0a7b1e598b1345ae5
KadirZukorlic <kadirzukorlic@gmail.com>
2024-02-16 03:10:09 +0100

    Add post route

--- src/routes/api/comments/+server.js
+++ src/routes/api/comments/+server.js
@@ -1,12 +1,19 @@
-import {comments} from '$lib/comments'
-import {json} from "@sveltejs/kit";
+import { comments } from '$lib/comments'
+import { json } from '@sveltejs/kit'
 
 export const GET = () => {
-    // return new Response(JSON.stringify(comments, {
-    //    headers: {
-    //        'Content-Type': 'application/json'
-    //    }
-    // }))
+  // return new Response(JSON.stringify(comments, {
+  //    headers: {
+  //        'Content-Type': 'application/json'
+  //    }
+  // }))
+  return json(comments)
+}
 
-    return json(comments)
+export const POST = async ({ request }) => {
+  const { text } = await request.json()
+  comments.push({ id: comments.length + 1, text })
+  return new Response(JSON.stringify(comments), {
+    statusCode: 201,
+  })
 }
