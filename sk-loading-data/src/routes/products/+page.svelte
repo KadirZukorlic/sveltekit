5a848437ed81c80821e7cd7976c73f933ac6566c
KadirZukorlic <kadirzukorlic@gmail.com>
2024-03-09 01:05:33 +0100

    Add preload data on hover

--- sk-loading-data/src/routes/products/+page.svelte
+++ sk-loading-data/src/routes/products/+page.svelte
@@ -9,7 +9,9 @@
 <p>list of products</p>
 {#each products as product}
     <div>
-        <h2>{product.title}</h2>
+        <a href={`/products/${product.id}`}>
+            <h2>{product.title}</h2>
+        </a>
         <p>{product.description}</p>
         <hr />
         <p>{product.price} $</p>
